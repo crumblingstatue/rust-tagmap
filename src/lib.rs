443e860ce76e189ebe0bf04f5b916088ca1d56d6
Mika Attila <radiantstatue@gmail.com>
2015-10-05 23:57:21 +0200

    New iterator based request API + add some tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![warn(missing_docs)]
 
 use std::collections::HashMap;
+use std::collections::hash_map::Iter;
 use std::hash::Hash;
 
 /// A container that allows item lookup based on tag matching.
@@ -12,28 +13,68 @@ pub struct TagMap<T: Eq + Hash, TAG: Eq> {
     pub entries: HashMap<T, Vec<TAG>>,
 }
 
+/// Iterator over entries matching requested tags.
+pub struct Matching<'hi, 'rt, T: 'static, TAG: 'static> {
+    iter: Iter<'hi, T, Vec<TAG>>,
+    requested_tags: &'rt [TAG],
+}
+
+impl<'a, 'b, T: 'a, TAG: 'a + Eq> Iterator for Matching<'a, 'b, T, TAG> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.iter.next() {
+                Some((v, tags)) => {
+                    let mut count = 0;
+                    for req in self.requested_tags {
+                        for tag in tags {
+                            if tag == req {
+                                count += 1;
+                            }
+                        }
+                    }
+                    if count == self.requested_tags.len() {
+                        return Some(v);
+                    }
+                }
+                None => return None,
+            }
+        }
+    }
+}
+
 impl<T: Eq + Hash, TAG: Eq> TagMap<T, TAG> {
     /// Creates a new empty TagMap.
     pub fn new() -> Self {
         TagMap { entries: HashMap::new() }
     }
-    /// Returns the entries matching the given tags.
-    pub fn matching(&self, tags: &[TAG]) -> Vec<&T> {
-        let mut vec = Vec::new();
-        'entries: for (k, v) in &self.entries {
-            for tag in tags.iter() {
-                let mut has_tag = false;
-                for entry_tag in v.iter() {
-                    if tag == entry_tag {
-                        has_tag = true;
-                    }
-                }
-                if !has_tag {
-                    continue 'entries;
-                }
-            }
-            vec.push(k);
+    /// Returns the entries matching the requested tags.
+    pub fn matching<'s, 't>(&'s self, tags: &'t [TAG]) -> Matching<'s, 't, T, TAG> {
+        Matching {
+            iter: self.entries.iter(),
+            requested_tags: tags,
         }
-        vec
     }
+}
+
+#[test]
+fn test() {
+    let mut map = TagMap::new();
+    map.entries.insert("elephant",
+                       vec!["mammal", "herbivore", "large", "smart"]);
+    map.entries.insert("mouse", vec!["mammal", "herbivore", "small"]);
+    map.entries.insert("snake", vec!["reptile", "carnivore", "poisonous"]);
+    map.entries.insert("shark", vec!["fish", "carnivore", "large"]);
+    map.entries.insert("human", vec!["mammal", "omnivore", "smart"]);
+    macro_rules! check {
+        ($tags:expr, $expected:expr) => {{
+            let mut v: Vec<_> = map.matching($tags).collect();
+            v.sort();
+            let mut expected = $expected;
+            expected.sort();
+            assert_eq!(&v[..], expected);
+        }}
+    }
+    check!(&["mammal"], [&"human", &"elephant", &"mouse"]);
+    check!(&["carnivore"], [&"snake", &"shark"]);
 }
