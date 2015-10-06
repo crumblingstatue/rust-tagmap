65c8da5239d8fc46aff1a98a4bd0a9f942644b19
Mika Attila <radiantstatue@gmail.com>
2015-10-06 10:58:30 +0200

    Implement rule matching

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,72 @@ pub struct TagMap<T: Eq + Hash, TAG: Eq> {
     pub entries: HashMap<T, Vec<TAG>>,
 }
 
-/// Iterator over entries matching requested tags.
-pub struct Matching<'hi, 'rt, T: 'static, TAG: 'static> {
+/// Iterator over entries matching a rule.
+pub struct Matching<'hi, 'r, T: 'static, TAG: 'static> {
     iter: Iter<'hi, T, Vec<TAG>>,
-    requested_tags: &'rt [TAG],
+    rule: &'r MatchRule<TAG>,
+}
+
+fn tags_match_rule<TAG: Eq>(tags: &[TAG], rule: &MatchRule<TAG>) -> bool {
+    use MatchRule::*;
+    match rule {
+        &Tags(ref m_tags) => {
+            let mut count = 0;
+            for m_tag in m_tags {
+                for tag in tags {
+                    if *tag == *m_tag {
+                        count += 1;
+                    }
+                }
+            }
+            count == m_tags.len()
+        }
+        &NotTags(ref m_tags) => {
+            for m_tag in m_tags {
+                for tag in tags {
+                    if *tag == *m_tag {
+                        return false;
+                    }
+                }
+            }
+            true
+        }
+        &AnyTag(ref m_tags) => {
+            for m_tag in m_tags {
+                for tag in tags {
+                    if *tag == *m_tag {
+                        return true;
+                    }
+                }
+            }
+            false
+        }
+        &Rules(ref rules) => {
+            let mut count = 0;
+            for rule in rules {
+                if tags_match_rule(tags, rule) {
+                    count += 1;
+                }
+            }
+            count == rules.len()
+        }
+        &NotRules(ref rules) => {
+            for rule in rules {
+                if tags_match_rule(tags, rule) {
+                    return false;
+                }
+            }
+            true
+        }
+        &AnyRule(ref rules) => {
+            for rule in rules {
+                if tags_match_rule(tags, rule) {
+                    return true;
+                }
+            }
+            false
+        }
+    }
 }
 
 impl<'a, 'b, T: 'a, TAG: 'a + Eq> Iterator for Matching<'a, 'b, T, TAG> {
@@ -25,16 +87,10 @@ impl<'a, 'b, T: 'a, TAG: 'a + Eq> Iterator for Matching<'a, 'b, T, TAG> {
         loop {
             match self.iter.next() {
                 Some((v, tags)) => {
-                    let mut count = 0;
-                    for req in self.requested_tags {
-                        for tag in tags {
-                            if tag == req {
-                                count += 1;
-                            }
-                        }
-                    }
-                    if count == self.requested_tags.len() {
+                    if tags_match_rule(tags, self.rule) {
                         return Some(v);
+                    } else {
+                        continue;
                     }
                 }
                 None => return None,
@@ -48,24 +104,54 @@ impl<T: Eq + Hash, TAG: Eq> TagMap<T, TAG> {
     pub fn new() -> Self {
         TagMap { entries: HashMap::new() }
     }
-    /// Returns the entries matching the requested tags.
-    pub fn matching<'s, 't>(&'s self, tags: &'t [TAG]) -> Matching<'s, 't, T, TAG> {
+    /// Returns the entries matching the given rule.
+    pub fn matching<'s, 'r>(&'s self, rule: &'r MatchRule<TAG>) -> Matching<'s, 'r, T, TAG> {
         Matching {
             iter: self.entries.iter(),
-            requested_tags: tags,
+            rule: rule,
         }
     }
 }
 
+/// A rule of how to match against tags.
+pub enum MatchRule<TAG> {
+    /// Match all given tags.
+    Tags(Vec<TAG>),
+    /// Don't match any given tag.
+    NotTags(Vec<TAG>),
+    /// Match any given tag.
+    AnyTag(Vec<TAG>),
+    /// Match all given rules.
+    Rules(Vec<MatchRule<TAG>>),
+    /// Don't match any given rule.
+    NotRules(Vec<MatchRule<TAG>>),
+    /// Match any given rule.
+    AnyRule(Vec<MatchRule<TAG>>),
+}
+
 #[test]
 fn test() {
+    use MatchRule::*;
     let mut map = TagMap::new();
     map.entries.insert("elephant",
-                       vec!["mammal", "herbivore", "large", "smart"]);
-    map.entries.insert("mouse", vec!["mammal", "herbivore", "small"]);
-    map.entries.insert("snake", vec!["reptile", "carnivore", "poisonous"]);
-    map.entries.insert("shark", vec!["fish", "carnivore", "large"]);
-    map.entries.insert("human", vec!["mammal", "omnivore", "smart"]);
+                       vec!["mammal", "herbivore", "large", "intelligent", "friendly"]);
+    map.entries.insert("mouse",
+                       vec!["mammal", "herbivore", "small", "furry", "neutral"]);
+    map.entries.insert("snake",
+                       vec!["reptile", "carnivore", "poisonous", "hostile"]);
+    map.entries.insert("shark",
+                       vec!["fish", "carnivore", "large", "hostile"]);
+    map.entries.insert("human",
+                       vec!["mammal", "omnivore", "intelligent", "friendly", "primate"]);
+    map.entries.insert("lion",
+                       vec!["feline", "mammal", "carnivore", "hostile", "furry"]);
+    map.entries.insert("dog",
+                       vec!["canine", "mammal", "carnivore", "friendly", "furry"]);
+    map.entries.insert("chimpanzee",
+                       vec!["mammal", "primate", "neutral", "omnivore", "furry"]);
+    map.entries.insert("goldfish", vec!["fish", "friendly"]);
+    map.entries.insert("carp", vec!["fish", "neutral"]);
+    map.entries.insert("blowfish", vec!["fish", "poisonous"]);
     macro_rules! check {
         ($tags:expr, $expected:expr) => {{
             let mut v: Vec<_> = map.matching($tags).collect();
@@ -75,6 +161,23 @@ fn test() {
             assert_eq!(&v[..], expected);
         }}
     }
-    check!(&["mammal"], [&"human", &"elephant", &"mouse"]);
-    check!(&["carnivore"], [&"snake", &"shark"]);
+    check!(&Tags(vec!["mammal"]),
+           [&"human", &"elephant", &"mouse", &"dog", &"lion", &"chimpanzee"]);
+    check!(&Tags(vec!["carnivore", "mammal", "friendly"]),
+           [&"dog"]);
+    check!(&NotTags(vec!["mammal"]),
+           [&"snake", &"shark", &"goldfish", &"carp", &"blowfish"]);
+    check!(&Rules(vec![Tags(vec!["fish"]), NotTags(vec!["poisonous"])]),
+           [&"goldfish", &"carp", &"shark"]);
+    check!(&AnyTag(vec!["canine", "reptile"]),
+           [&"dog", &"snake"]);
+    check!(&AnyRule(vec![
+            Rules(vec![
+                Tags(vec!["carnivore"]), NotTags(vec!["friendly"]),
+            ]),
+            Rules(vec![
+                Tags(vec!["fish"]), AnyTag(vec!["friendly", "neutral", "poisonous"])
+            ]),
+        ]),
+           [&"shark", &"lion", &"goldfish", &"carp", &"blowfish", &"snake"])
 }
